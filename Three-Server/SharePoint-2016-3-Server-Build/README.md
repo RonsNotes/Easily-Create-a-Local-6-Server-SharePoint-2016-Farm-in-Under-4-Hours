021d49ce91f00e5d71005a064d9eb2e2bac7a225
Rocky <mark@bluebuffalopress.com>
2017-01-12 17:56:39 -0800

    Add files via upload

---
+++ Three-Server/SharePoint-2016-3-Server-Build/README.md
@@ -0,0 +1,2 @@
+# SharePoint-2016-3-Server-Build
+Scripts to build a 3 server farm.
---
+++ Three-Server/SharePoint-2016-3-Server-Build/SPApp Scripts/04 Turn off firewall.ps1
@@ -0,0 +1,23 @@
+﻿# *************************************************************************
+#
+# Script Name: Turn off firewall.ps1
+# PS Version: 5.1
+# Author: Ron Davis
+# 
+# Last Modified: 12/23/2016
+# 
+# Description: This script turns off the firewall.         
+#
+#
+# *************************************************************************
+
+#Verify PowerShell is running as Administrator
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+If (!( $isAdmin )) {
+	Write-Host "-- Restarting as Administrator" -ForegroundColor Cyan ; Sleep -Seconds 1
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs 
+	exit
+}
+
+#TASK: TURN OFF THE FIREWALL
+Set-NetFirewallProfile -Profile domain,Public,Private -Enabled False 

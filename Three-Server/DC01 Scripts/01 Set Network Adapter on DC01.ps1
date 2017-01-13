9a3ce1dc19f5835f049c5bca7ac47656b3c11bdf
Rocky <mark@bluebuffalopress.com>
2017-01-12 18:05:55 -0800

    Add files via upload

---
+++ Three-Server/DC01 Scripts/01 Set Network Adapter on DC01.ps1
@@ -0,0 +1,38 @@
+﻿# *************************************************************************
+#
+# Script Name: Set Network Adapter on DC01.ps1
+# PS Version: 5.1
+# Author: Ron Davis
+# 
+# Last Modified: 12/23/2016
+# 
+# Description: This script sets the Network Adapter on the DC01 VM.         
+#
+#
+# *************************************************************************
+
+#Verify PowerShell is running as Administrator
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+If (!( $isAdmin )) {
+	Write-Host "-- Restarting as Administrator" -ForegroundColor Cyan ; Sleep -Seconds 1
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs 
+	exit
+}
+
+#TASK: CONFIGURE NETWORK ADAPTER ON DC01
+$IP = "10.0.0.1"
+$MaskBits =  24 # This means subnet mask = 255.255.255.0
+
+$Dns = "10.0.0.1"
+$IPType = "IPv4"# Retrieve the network adapter that you want to configure
+$NA = Get-NetAdapter | where status -EQ up
+$Na.Name
+# Configure the IP address and default gateway
+New-NetIPAddress -InterfaceAlias $NA.Name -AddressFamily $IPType -IPAddress $IP -PrefixLength $MaskBits 
+   
+    `
+# Configure the DNS client server IP addresses
+Get-NetAdapter -Name $NA.Name | Set-DnsClientServerAddress -ServerAddresses $DNS
+Ping 10.0.0.1
+Ping 10.0.0.1
+ipconfig
---
+++ Three-Server/Host/04 Add External Adapter back Three Servers.ps1
@@ -0,0 +1,36 @@
+﻿# *************************************************************************
+#
+# Script Name: Add External Adapter back Three Servers.ps1
+# PS Version: 5.1
+# Author: Ron Davis
+# 
+# Last Modified: 12/23/2016
+# 
+# Description: This script adds an External Network Adapter
+# and attatches the SQL ISO file to the SQL VM.           
+#
+# *************************************************************************
+
+#Verify PowerShell is running as Administrator
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+If (!( $isAdmin )) {
+	Write-Host "-- Restarting as Administrator" -ForegroundColor Cyan ; Sleep -Seconds 1
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs 
+	exit
+}
+
+$hOLD = @(Get-Content  "C:\RonsNotes\VMNames\AllVMs\AllVMs.txt" )
+$VMs = $hOLD -split ","
+$VMs
+$VMs | Stop-vm 
+
+$VMs | Add-VMNetworkAdapter -SwitchName VMExternalNetwork 
+
+
+Set-VMDvdDrive -VMName SQLSvr -Path C:\RonsNotes\ISOs\SQL2016\SQLServer2016-x64-ENU.iso
+
+Function DropAndCreateVirtualSwitch{
+#This step prevents conflicts if the switch already exists
+
+
+} #End

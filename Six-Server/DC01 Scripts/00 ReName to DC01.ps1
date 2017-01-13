fc04dc5ae5b1a79519dcf3f419844fd4461ce2c3
Rocky <mark@bluebuffalopress.com>
2017-01-12 17:55:54 -0800

    Add files via upload

---
+++ Six-Server/DC01 Scripts/00 ReName to DC01.ps1
@@ -0,0 +1,24 @@
+﻿# *************************************************************************
+#
+# Script Name: ReName to DC01.ps1
+# PS Version: 5.1
+# Author: Ron Davis
+# 
+# Last Modified: 12/23/2016
+# 
+# Description: This script renames DC01 and restarts the VM.          
+#
+#
+# *************************************************************************
+
+
+#Verify PowerShell is running as Administrator
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+If (!( $isAdmin )) {
+	Write-Host "-- Restarting as Administrator" -ForegroundColor Cyan ; Sleep -Seconds 1
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs 
+	exit
+}
+
+#TASK: RENAMING DC01
+Rename-Computer -NewName DC01 -Restart
---
+++ Six-Server/SPWeb Scripts/00 Set Network Adapter.ps1
@@ -0,0 +1,39 @@
+﻿# *************************************************************************
+#
+# Script Name: Set Network Adapter.ps1
+# PS Version: 5.1
+# Author: Ron Davis
+# 
+# Last Modified: 12/23/2016
+# 
+# Description: This script sets up a network adapter.         
+#
+#
+# *************************************************************************
+
+#Verify PowerShell is running as Administrator
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+If (!( $isAdmin )) {
+	Write-Host "-- Restarting as Administrator" -ForegroundColor Cyan ; Sleep -Seconds 1
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs 
+	exit
+}
+
+#TASK: CONFIGURE NETWORK ADAPTER
+$IP = "10.0.0.6"
+$MaskBits =  24 # This means subnet mask = 255.255.255.0
+
+$Dns = "10.0.0.1"
+$IPType = "IPv4"# Retrieve the network adapter that you want to configure
+$NA = Get-NetAdapter | where status -EQ up
+$Na.Name
+ # Configure the IP address and default gateway
+New-NetIPAddress -InterfaceAlias $NA.Name -AddressFamily $IPType -IPAddress $IP -PrefixLength $MaskBits 
+   
+    `
+# Configure the DNS client server IP addresses
+
+Get-NetAdapter -Name $NA.Name | Set-DnsClientServerAddress -ServerAddresses $DNS
+Ping 10.0.0.1
+Ping DC01
+

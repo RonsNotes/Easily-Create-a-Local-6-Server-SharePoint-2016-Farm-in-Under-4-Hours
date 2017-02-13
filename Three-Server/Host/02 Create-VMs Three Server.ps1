234ff5a4bfce6951cb5139f81d828a4de8de7b73
Rocky <Mark@BlueBuffaloPress.com>
2017-02-13 11:19:57 -0800

    Script Modification

--- Three-Server/Host/02 Create-VMs Three Server.ps1
+++ Three-Server/Host/02 Create-VMs Three Server.ps1
@@ -19,31 +19,6 @@ If (!( $isAdmin )) {
 	exit
 }
 
-#TASK: VERIFYING THE CORRECT FILES ARE IN THE PROPER LOCATION
-Function Test-CorrectFiles {
-$ISOSvr2016 = Get-ChildItem C:\RonsNotes\ISOs\Server2016\ | %{$_.Name} -ErrorAction inquire # ASK
-$ISOSql = Get-ChildItem C:\RonsNotes\ISOs\SQL2016\ | %{$_.Name} -ErrorAction inquire # ASK
-$IMGSharePoint2016 = Get-ChildItem C:\RonsNotes\ISOs\Sharepoint2016\ | %{$_.Name} -ErrorAction inquire # ASK
-
-If ($ISOSvr2016 -eq "14393.0.160715-1616.RS1_RELEASE_SERVER_EVAL_X64FRE_EN-US.ISO")
-{Write-Host "Expected version of ISO for Server 2016 in expected location" -ForegroundColor Green}
-Else {Write-Host "Wrong or missing Server 2016 ISO. 14393.0.160715-1616.RS1_RELEASE_SERVER_EVAL_X64FRE_EN-US.ISO expected Correct to continue " -ForegroundColor Red; Read-Host -Prompt "Any key to continue" }
-
-If ($ISOSQL -eq "SQLServer2016-x64-ENU.iso")
-{Write-Host "Expected version of IMG for SQLServer 2016 in expected location" -ForegroundColor Green}
-Else {Write-Host "Wrong or missing SQL 2016 ISO. SQLServer2016-x64-ENU.iso expected. Correct to continue " -ForegroundColor Red ;  Read-Host -Prompt "Any key to continue"}
-
-If ($IMGSharePoint2016 -eq "officeserver.img")
-{Write-Host "Expected version of IMG for SharePoint Svr 2016 in expected location" -ForegroundColor Green}
-Else {Write-Host "Wrong or missing SharePoint.IMG. officeserver.img expected. Correct to continue " -ForegroundColor Red ;  Read-Host -Prompt "Any key to continue"}
-
-Write-host "*******"
-Write-host "*******"
-Write-host "*******"
-Write-host "*******"
-
-}
-Test-CorrectFiles
 
 #The following three code blocks create the virtual machines.
 #TASK: CREATE DC01
